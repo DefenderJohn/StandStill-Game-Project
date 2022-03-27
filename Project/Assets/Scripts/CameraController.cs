475e85884e40832de095d362e0d53b3ceca7725c
DefenderJohn <1175745126@qq.com>
2022-03-26 20:14:53 -0600

    由于新版本中存在部分问题，现对整个项目用2019版本进行完全重构。

--- Project/Assets/Scripts/CameraController.cs
+++ Project/Assets/Scripts/CameraController.cs
@@ -1,18 +1,30 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float ratio = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Rotate(Vector3.down * ratio);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Rotate(Vector3.up * ratio);
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += this.transform.forward * ratio;
+        if (Input.GetKey(KeyCode.S))
+            this.transform.position += this.transform.forward * -ratio;
+        if (Input.GetKey(KeyCode.LeftShift))
+            this.transform.position += this.transform.up * ratio;
+        if (Input.GetKey(KeyCode.LeftControl))
+            this.transform.position += this.transform.up * -ratio;
     }
 }
