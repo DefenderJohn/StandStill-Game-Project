2a7a39e8a7fcbf0c061c2d84a4f08d798114cee8
DefenderJohn <1175745126@qq.com>
2022-04-29 23:59:11 -0600

    让血条实现摄像机透视角

--- Project/Assets/Scripts/Controllers/TankController.cs
+++ Project/Assets/Scripts/Controllers/TankController.cs
@@ -12,15 +12,18 @@ public class TankController : MonoBehaviour
     public float attack;
     public float aimingTime;
     public float loadingTime;
+    public Canvas canvas;
     public Slider HPSlider;
     public Slider ammoSlider;
     public Slider fuelSlider;
     public GameObject turrent;
+    public Camera mainCamera;
     public Vector3 lastframePos;
     public Vector3 currentSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = GameObject.Find("Cameras").GetComponent<CameraController>().mainCamera;
         HPSlider.maxValue = this.hitPoints;
         HPSlider.minValue = 0;
         fuelSlider.maxValue = this.fuel;
@@ -32,9 +35,10 @@ public class TankController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        canvas.gameObject.transform.rotation = mainCamera.transform.rotation;
         currentSpeed = this.gameObject.transform.position - lastframePos;
         lastframePos = this.gameObject.transform.position;
-        
+
         float fuelConsumeHorizontal = Mathf.Sqrt(currentSpeed.x * currentSpeed.x + currentSpeed.z * currentSpeed.z);
         float fuelConsumeVertical = currentSpeed.y;
         float fuelConsume = fuelConsumeHorizontal * 0.5f * fuelConsumingRatio + fuelConsumeVertical * 1.5f * fuelConsumingRatio + 0.1f * fuelConsumingRatio;
